d9f0d107537c7b9a950e9acb5209d2f7cd6d8183
peki1309 <ndeyependaniang@esp.sn>
2025-04-07 23:03:30 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,3 +6,25 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+printf("8. Afficher un étudiant aléatoire\n");
+printf("0. Quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+           case 8:
+                afficherEtudiantRandom(tab, n);
+                break;
+                
+            case 0:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Choix invalide.\n");
+        }
+    } while (choix != 0);
+
+
+    return 0;
+}
