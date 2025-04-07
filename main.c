76064fbb523d160aea94f9da9bdf3650c47b22c2
Souleymane KONE <168046188+gitsouley@users.noreply.github.com>
2025-04-07 16:44:32 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include "fonction.h"
+#include <stdlib.h>
+#include <string.h>
+#include "fonctions.h"
+#define NBETU 100
 
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(void){
+  //Declaration 
+  Etudiant *VETU[NBETU] = {NULL};
+  restaurerListe(VETU, "etudiants.txt");
+ 
+  saisirEtudiant(VETU);
+  afficherListe(VETU);
+  supprimerEtudiant(VETU);
+  afficherListe(VETU);
+  
+
+  sauvegarderListe(VETU, "etudiants.txt");
+  nettoyerMemoire(VETU);
 }
