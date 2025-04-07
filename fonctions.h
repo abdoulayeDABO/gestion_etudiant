9b50cdcae21dad334d197ac41fbe12d75687098d
peki1309 <ndeyependaniang@esp.sn>
2025-04-07 23:30:47 +0000

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,5 +1,18 @@
+#ifndef ETUDIANTS_H
+#define ETUDIANTS_H
+
+#define MAX_ETUDIANTS 100
+#define TAILLE_NOM 50
+
+typedef struct {
+    int numero;
+    char nom[TAILLE_NOM];
+    float note;
+} Etudiant;
 
 
-
-
+// Prototype de la fonction afficherEtudiantRandom
 void afficherEtudiantRandom(const Etudiant tab[], int n);
+
+#endif
+
