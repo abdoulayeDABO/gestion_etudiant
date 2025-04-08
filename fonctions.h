e20645054e6be4ffb991394184131668a5a91bc7
ndeye532 <ndeyegueye81004@gmail.com>
2025-04-08 18:50:36 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,2 +1,13 @@
-/ Déclaration de la fonction afficherEtudiantsParMerite
+#ifndef FONCTION_H
+#define FONCTION_H
+
+// Déclarations des structures
+typedef struct {
+    int numero;   // Numéro de l'étudiant
+    char nom[50]; // Nom de l'étudiant
+    float note;   // Note de l'étudiant
+} Etudiant;
+
+// Déclaration de la fonction afficherEtudiantsParMerite
 void afficherEtudiantsParMerite(Etudiant VETU[], int SUIVANT[], int NBETU);
+#endif // FONCTION_H
