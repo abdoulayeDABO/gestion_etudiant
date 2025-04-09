1e91f542560c47a29454e380c0d5feb26299322b
ndeye532 <ndeyegueye81004@gmail.com>
2025-04-09 16:03:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "fonction.h"
 
+#define NBETU 100
 int main() {
     Etudiant VETU[NBETU];
     int SUIVANT[NBETU];
     int NBETU = 0;
+
+    // Charger les étudiants depuis le fichier de sauvegarde
+    ChargerEtudiants(VETU, SUIVANT, &NBETU);
 
     printf("Affichage des étudiants par ordre de mérite :\n");
     afficherEtudiantsParMerite(VETU, SUIVANT, NBETU);
 
     return 0;
 }
+
