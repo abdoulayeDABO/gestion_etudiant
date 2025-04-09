6ac03cb6be87f4cea16b6e0558fd9e43bd90deaa
ndeye532 <ndeyegueye81004@gmail.com>
2025-04-09 16:04:44 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -13,3 +13,29 @@ void afficherEtudiantsParMerite(Etudiant VETU[], int SUIVANT[], int NBETU) {
         i = SUIVANT[i];  // On passe à l'étudiant suivant dans la chaîne
     }
 }
+void ChargerEtudiants(Etudiant VETU[], int SUIVANT[], int* NBETU) {
+    FILE* file = fopen("etudiants.dat", "rb");
+    if (file == NULL) {
+        printf("Aucun fichier de sauvegarde trouvé.\n");
+        return;
+    }
+
+    // Lire l'indice du premier étudiant (DEB)
+    int DEB;
+    fread(&DEB, sizeof(int), 1, file);
+
+    // Initialiser le nombre d'étudiants
+    *NBETU = 0;
+    
+    // Charger les étudiants et les lier avec SUIVANT
+    int i = 0;
+    while (fread(&VETU[i], sizeof(Etudiant), 1, file) == 1) {
+        fread(&SUIVANT[i], sizeof(int), 1, file);  // Lire l'indice du suivant
+        i++;
+        (*NBETU)++;  // Incrémenter le nombre d'étudiants
+    }
+
+    fclose(file);
+    printf("Les données des étudiants ont été chargées avec succès.\n");
+}
+
